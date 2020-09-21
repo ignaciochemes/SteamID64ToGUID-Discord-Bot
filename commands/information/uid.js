94851647c9665334fcc49c6209c95c1bdf297570
siegmund0 <54911521+siegmund0@users.noreply.github.com>
2020-09-21 17:01:16 -0300

    SC

--- commands/information/uid.js
+++ commands/information/uid.js
@@ -9,35 +9,45 @@ module.exports = {
     category: "information",
     description: "Returns the Hash request (Steam Id 64 to base64 hash UID)",
     run: async(client, message, args) => {
-		    let discordUserTag = message.member.user.tag;
+        let discordUserTag = message.member.user.tag;
         let discordServer = message.guild.name;
         let tmp = message.content.split(" ");
         let pwd = `${args}`;
         let siEnviarEmbed = new Discord.MessageEmbed();
-			if(!args[0]) return message.reply(`Insert account id64 | -uid <your id64 here> | -uid 765611981... \nIf you dont have your steam id 64 number, please execute the following command\n\`-steam <your-steam-profile-link>\`\nExample -steam https://steamcommunity.com/id/siegmundsensi/`)
-			.then(msg => {
-			  msg.delete({ timeout: 25000 })
-			});
-            try {
-                let hash = crypto.createHash('sha256').update(pwd).digest('base64');
-				
-                    let sql = `INSERT INTO uids (id64, uid, discordserver, usuariotag) VALUES ('${args}', '${hash}', '${discordServer}', '${discordUserTag}')`;
-                    con.query(sql, function (err, result) {
-                      if (err) throw err;
-                      console.log("1 record inserted");
-                    });
-				
-                console.log(`Conversion de Guid exitosa`);
-                siEnviarEmbed.setDescription("<@" + message.author.id + ">")
-					.addField('Your UID encryption is:', `${hash}`, true)
-					.setColor("#F8C300")
-					.setFooter(`2020 © Id64ToGuid | Bohemia Interactive - Battleye | siegmund - oaki`)
-            } catch(e)
-            {
-                 console.log(`Error al convertir UID ${e}`);
-                 siEnviarEmbed.setTitle(`Error converting`)
-                 .setColor("#A62019")
-                 .setDescription(`Are you sure you entered a correct number? \nExecute -steam and enter your Steam Link.\nYou have to find your SteamId64 765611 .... and then, use it with the command \`-uid 765611.....\` to return the hash.`)
-            } finally {message.channel.send(siEnviarEmbed)}
+        if (!args[0]) return message.reply(`Insert account id64 | -uid <your id64 here> | -uid 765611981... \nIf you dont have your steam id 64 number, please execute the following command\n\`-steam <your-steam-profile-link>\`\nExample -steam https://steamcommunity.com/id/siegmundsensi/`)
+            .then(msg => {
+                msg.delete({ timeout: 25000 })
+            });
+        try {
+            const mas = /\+/g;
+            const guion = '-';
+            const barra = /\//g;
+            const guionBajo = '_';
+            let hash = crypto.createHash('sha256').update(pwd).digest('base64');
+            let tomaHash = hash.replace(mas, guion);
+            let reemplazaHash = tomaHash.replace(barra, guionBajo);
+
+            // function replaceAll(string, search, replace) {
+            //     return string.split(search).join(replace);
+            // }
+            // replaceAll(hash, '+', '-');
+
+            let sql = `INSERT INTO uids (id64, uid, discordserver, usuariotag) VALUES ('${args}', '${hash}', '${discordServer}', '${discordUserTag}')`;
+            con.query(sql, function(err, result) {
+                if (err) throw err;
+                console.log("1 record inserted");
+            });
+
+            console.log(`Conversion de Guid exitosa`);
+            siEnviarEmbed.setDescription("<@" + message.author.id + ">")
+                .addField('Your UID encryption is:', `✅ Works: \`${reemplazaHash}\` \n\n⁉️ Test: \`${hash}\``, true)
+                .setColor("#F8C300")
+                .setFooter(`2020 © Id64ToGuid | Bohemia Interactive - Battleye | siegmund - oaki`)
+        } catch (e) {
+            console.log(`Error al convertir UID ${e}`);
+            siEnviarEmbed.setTitle(`Error converting`)
+                .setColor("#A62019")
+                .setDescription(`Are you sure you entered a correct number? \nExecute -steam and enter your Steam Link.\nYou have to find your SteamId64 765611 .... and then, use it with the command \`-uid 765611.....\` to return the hash.`)
+        } finally { message.channel.send(siEnviarEmbed) }
     }
 }
